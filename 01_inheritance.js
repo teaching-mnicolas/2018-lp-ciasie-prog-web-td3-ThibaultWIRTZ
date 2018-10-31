3c0b5d26f03c6f180a80efef2b3dfdee29a2616a
ThibaultWIRTZ <wirtzthibault@gmail.com>
2018-10-31 08:31:43 +0100

    commit sans echec

--- 01_inheritance.js
+++ 01_inheritance.js
@@ -3,24 +3,24 @@ describe ("Inheritance", function() {
   it ("object has a constructor object", function() {
     const Character = function() {};
     const warrior = new Character();
-    expect(warrior.constructor).toBe(__);
+    expect(warrior.constructor).toBe(Character.prototype.constructor);
   });
 
   it ("function has a prototype object", function() {
     const Character = function() {};
-    expect(Character.__).toBeDefined();   // check the prototype property
+    expect(Character.constructor).toBeDefined();   // check the prototype property
   });
 
   it ("function prototype has a constructor that is itself", function() {
     const Character = function() {};
     expect(Character.prototype.constructor).toBeDefined();
-    expect(Character.prototype.constructor).toBe(__);
+    expect(Character.prototype.constructor).toBe(Character.prototype.constructor);
   });
 
   it ("object is instance of its constructor", function() {
     const Character = function() {};
     const warrior = new Character();
-    expect(warrior instanceof __).toBeTruthy();   // warrior is instance of its constructor
+    expect(warrior instanceof Character).toBeTruthy();   // warrior is instance of its constructor
   });
 
 });
--- 03_constructors.js
+++ 03_constructors.js
@@ -1,31 +1,31 @@
 describe ("Constructors", function() {
 
   it ("Object is constructor of {}", function() {
-    expect({}.constructor).toBe(__);
+    expect({}.constructor).toBe(Object.prototype.constructor);
   });
 
   it ("Array is constructor of []", function() {
-    expect([].constructor).toBe(__);
+    expect([].constructor).toBe(Array.prototype.constructor);
   });
 
   it ("String is constructor of ''", function() {
-    expect("".constructor).toBe(__);
+    expect("".constructor).toBe(String.prototype.constructor);
   });
 
   it ("Number is constructor of 1", function() {
-    expect((1).constructor).toBe(__);
+    expect((1).constructor).toBe(Number.prototype.constructor);
   });
 
   it ("Boolean is constructor of true", function() {
-    expect(true.constructor).toBe(__);
+    expect(true.constructor).toBe(Boolean.prototype.constructor);
   });
 
   it ("RegExp is constructor of /aaa/", function() {
-    expect(/aaa/.constructor).toBe(__);
+    expect(/aaa/.constructor).toBe(RegExp.prototype.constructor);
   });
 
   it ("Function is constructor of a function", function() {
     const f = function() {};
-    expect(f.constructor).toBe(__);
+    expect(f.constructor).toBe(Function.prototype.constructor);
   });
 });
